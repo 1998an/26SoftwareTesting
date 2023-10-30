ae32ac784eba73cad351ae2b7041165dc7a03e52
ankit <ankit@Windows11>
2023-10-30 17:55:21 +0530

    first project

---
+++ src/test/java/testCases/CartPageTest.java
@@ -0,0 +1,95 @@
+package testCases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import base.TestBase;
+import pages.CartPage;
+import pages.InventoryPage;
+import pages.LoginPage;
+import utility.CaptureScreenShot;
+import utility.ReadData;
+
+public class CartPageTest extends TestBase
+{
+	LoginPage login;
+	InventoryPage invent;
+	CartPage cart;
+	@BeforeMethod(alwaysRun = true)
+	public void setUp() throws InterruptedException, IOException
+	{
+		initialization();
+		login=new LoginPage();
+		invent=new InventoryPage();
+		cart=new CartPage();
+		login.LoginToApplication();
+		invent.add6Product();
+		invent.clickCartCount();
+	}    
+	@Test(enabled = true,priority = 0,groups = {"sanity"})
+	public void verifyURLofCartPageTest() throws EncryptedDocumentException, IOException
+	{
+		String expUrl=ReadData.readExce_cart(0,0); //https://www.saucedemo.com/cart.html
+		String actUrl=cart.verifyURLofCartpage();
+		AssertJUnit.assertEquals(expUrl, actUrl);
+		Reporter.log("URL of cart page ="+actUrl);
+	}
+	@Test(enabled = true,priority = 1,dependsOnMethods = "verifyURLofCartPageTest",groups = {"sanity"})
+	public void verifytitleOfCartPageTest() throws EncryptedDocumentException, IOException
+	{
+		String expTitle=ReadData.readExce_cart(0,1); //Your Cart
+		String actTitle=cart.verifytitleOfCartPage();
+		AssertJUnit.assertEquals(expTitle, actTitle);
+		Reporter.log("Title of cart page ="+actTitle);
+	}
+	@Test(enabled = false,priority = 2,dependsOnMethods = "verifyURLofCartPageTest",groups = {"sanity"})
+	public void verifyQtyTextTest() throws EncryptedDocumentException, IOException
+	{
+		String expText=ReadData.readExce_cart(0,2); //QTY
+		String actText=cart.verifyQtyText();
+		AssertJUnit.assertEquals(expText, actText);
+		Reporter.log("Text of qty ="+actText);
+	}
+	@Test(enabled = false,priority = 3,dependsOnMethods = "verifyURLofCartPageTest",groups = {"sanity","regression"})
+	public void verifyDescriptionTextTest() throws EncryptedDocumentException, IOException
+	{
+		String expText=ReadData.readExce_cart(0,3); //Description
+		String actText=cart.verifyDescriptionText();
+		AssertJUnit.assertEquals(expText, actText);
+		Reporter.log("Text of Description ="+actText);
+	}
+	@Test(enabled = true,priority = 4,dependsOnMethods = "verifyURLofCartPageTest",groups = {"sanity","regression"})
+	public void clickCheckOutBtnTest() throws EncryptedDocumentException, IOException
+	{
+		String expURl=ReadData.readExce_cart(0,4); //https://www.saucedemo.com/checkout-step-one.html
+		String actUrl=cart.clickCheckOutBtn();
+		AssertJUnit.assertEquals(expURl, actUrl);
+		Reporter.log("Url after click on checkout button="+actUrl);
+	}
+	@Test(enabled = true,priority = 5,dependsOnMethods = "verifyURLofCartPageTest",groups = {"sanity","regression"})
+	public void clickContinueShoppingBtnTest() throws EncryptedDocumentException, IOException
+	{
+		String expURl=ReadData.readExce_cart(0,5); //https://www.saucedemo.com/inventory.html
+		String actUrl=cart.clickContinueShoppingBtn();
+		AssertJUnit.assertEquals(expURl, actUrl);
+		Reporter.log("Url after click on continue shopping button="+actUrl);
+	}
+	@AfterMethod(alwaysRun = true)
+	public void closeBrowser(ITestResult it) throws IOException
+	{
+		if(it.FAILURE == it.getStatus())
+		{
+			CaptureScreenShot.screenShot(it.getName());
+		}
+		driver.close();
+	}
+}
---
+++ src/test/java/testCases/InventoryPageTest.java
@@ -0,0 +1,156 @@
+package testCases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import base.TestBase;
+import pages.InventoryPage;
+import pages.LoginPage;
+import utility.CaptureScreenShot;
+import utility.ReadData;
+
+public class InventoryPageTest extends TestBase
+{
+	LoginPage login;
+	InventoryPage invent;
+	@BeforeMethod(alwaysRun = true)
+	public void setUp() throws InterruptedException, IOException
+	{
+		initialization();
+		login=new LoginPage();
+		invent=new InventoryPage();
+		login.LoginToApplication();
+	} 
+	@Test(enabled = true,priority = 0,groups= {"retest","sanity"})
+	public void verifyinventoryPageUrlTest() throws EncryptedDocumentException, IOException
+	{
+		String expURL=ReadData.readExcel_inventory(0,3);  //https://www.saucedemo.com/inventory.html
+		String actURL=invent.verifyinventoryPageUrl();
+		Assert.assertEquals(expURL, actURL);
+		Reporter.log("url of inventory page"+actURL);
+	}
+	@Test(enabled = true,priority = 3,dependsOnMethods = "verifyinventoryPageUrlTest",groups= {"retest","sanity"})
+	public void verifyProductLableTest() throws EncryptedDocumentException, IOException
+	{
+		String expLable=ReadData.readExcel_inventory(0,0);  //Products(0,0)
+		String actLable=invent.verifyProductLable();
+		AssertJUnit.assertEquals(expLable, actLable);
+		Reporter.log("Application Lable =" +actLable);
+	}
+	@Test(enabled = false,priority = 4,dependsOnMethods = "verifyinventoryPageUrlTest",groups= {"retest","sanity"})
+	public void verifyTwitterLogoTest()
+	{
+		boolean result=invent.verifyTwitterLogo();
+		AssertJUnit.assertEquals(result, true);
+		Reporter.log("Visibility of Twitter LOgo =" +result);
+	}
+	@Test(enabled = true,priority = 5,dependsOnMethods = "verifyinventoryPageUrlTest",groups= {"retest","sanity"})
+	public void verifyfacebookLogoTest()
+	{
+		boolean result=invent.verifyfacebookLogo();
+		AssertJUnit.assertEquals(result,true);
+		Reporter.log("Visibility of Facebook logo =" +result);
+	}
+	@Test(enabled = true,priority = 6,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity","regression"})
+	public void verifylinkedInLogoTest()
+	{
+		boolean result=invent.verifylinkedInLogo();
+		AssertJUnit.assertEquals(result,true);
+		Reporter.log("Visibility of linkedin logo =" +result);
+	}
+	@Test(enabled = false,priority = 7,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity","regression"})
+	public void verifyAppLogoTest()
+	{
+		boolean result = invent.verifyAppLogo();
+		AssertJUnit.assertEquals(result, true);
+		Reporter.log("Visiability of app logo ="+result);
+	}
+	@Test(enabled = true,priority = 8,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity","regression"})
+	public void verifyAddToCartBtn1Test()
+	{
+		 invent.verifyAddToCartBtn1();
+	}
+	@Test(enabled = false,priority = 9,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity","regression"})
+	public void verifyAddToCartBtn2Test()
+	{
+		invent.verifyAddToCartBtn2();
+	}
+	@Test(enabled = false,priority = 10,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity","regression"})
+	public void verifyAddToCartBtn3Test()
+	{
+		invent.verifyAddToCartBtn3();	
+	}
+	@Test(enabled = false,priority = 11,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity","regression"})
+	public void verifyAddToCartBtn4Test()
+	{
+		invent.verifyAddToCartBtn4();	
+	}
+	@Test(enabled = false,priority = 12,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity","regression"})
+	public void verifyAddToCartBtn5Test()
+	{
+		invent.verifyAddToCartBtn5();
+	}
+	@Test(enabled = false,priority = 13,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity"})
+	public void verifyAddToCartBtn6Test()
+	{
+		invent.verifyAddToCartBtn6();
+	}
+	@Test(enabled = false,priority = 14,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity"})
+	public void verifyProductSortBtnTest()
+	{
+		invent.verifyProductSortBtn();
+	}
+	@Test(enabled = true,priority = 15,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity"})
+	public void vedrifyProductCartLinkTest()
+	{
+		invent.vedrifyProductCartLink();
+	}
+	@Test(enabled = true,priority = 16,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity"})
+	public void verifyFooterTest()
+	{
+		invent.verifyFooter();
+	}
+	@Test(enabled = true,priority = 17,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity"})
+	public void burgerMenuBtnTest()
+	{
+		invent.burgerMenuBtn();
+	}
+	//use for add and count in shopping cart
+	@Test(enabled = true,priority = 18,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity"})
+	public void add6ProductTest() throws EncryptedDocumentException, IOException
+	{
+		String expCount=ReadData.readExcel_inventory(0,1);  //6
+		String actCount=invent.add6Product();
+		AssertJUnit.assertEquals(expCount, actCount);
+		Reporter.log("Total Product added in Shopping Cart= "+actCount);
+	}
+	//use for remove and count in shopping cart
+	@Test(enabled = true,priority = 2,dependsOnMethods = "verifyinventoryPageUrlTest",groups = {"sanity"})
+	public void remove2ProductTest() throws InterruptedException, EncryptedDocumentException, IOException
+	{
+		String expCount=ReadData.readExcel_inventory(0,2);  //4
+		String actCount=invent.remove2Product();
+		AssertJUnit.assertEquals(expCount, actCount);
+		Reporter.log("Total count in cart after removing 2 product ="+actCount);
+	}
+	@AfterMethod(alwaysRun = true)
+	public void closeBrowser(ITestResult it) throws IOException
+	{
+		if(it.FAILURE == it.getStatus())
+		{
+			CaptureScreenShot.screenShot(it.getName());
+		}
+		driver.close();
+	}
+		
+}
+	
