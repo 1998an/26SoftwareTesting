3aa8eca1d9018d6c1f92fa652db0747aecf48220
ankit <ankit@Windows11>
2023-11-07 15:54:00 +0530

    aa

--- src/test/java/testCases/verifyMultipleCredentialsTest.java
+++ src/test/java/testCases/verifyMultipleCredentialsTest.java
@@ -3,27 +3,22 @@ package testCases;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.Assert;
-import org.testng.AssertJUnit;
-import org.testng.AssertJUnit;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import base.TestBase;
 import pages.LoginPage;
 import utility.CaptureScreenShot;
-import utility.ReadData;
 
 public class verifyMultipleCredentialsTest extends TestBase
 {
 	LoginPage login;
 	
 
-	@BeforeMethod
+	@BeforeMethod(alwaysRun = true)
 	public void setUp()throws Exception
 	{
 		initialization();
@@ -54,7 +49,7 @@ public class verifyMultipleCredentialsTest extends TestBase
 				};
 		
 	}
-	@AfterMethod
+	@AfterMethod(alwaysRun = true)
 	public void closeBrowser(ITestResult it)throws Exception
 	{
 		if(it.FAILURE==it.getStatus())
