c5d553546ad2aeb3f45700d079a315c35bd10448
ankit <ankit@Windows11>
2023-11-12 00:36:34 +0530

    ii

--- src/test/java/testCases/LoginPageTest.java
+++ src/test/java/testCases/LoginPageTest.java
@@ -32,21 +32,21 @@ public class LoginPageTest extends TestBase
 	{
 		String expURL=ReadData.readExcel_login(0,0);  //https://www.saucedemo.com/
 		String actURL=login.verifyURLofApplication();
-		AssertJUnit.assertEquals(expURL, actURL);
+		Assert.assertEquals(expURL, actURL);
 	}
 	@Test(enabled = true)
 	public void verifyTitleOfApplication() throws EncryptedDocumentException, IOException
 	{
 		String expTitle=ReadData.readExcel_login(0,1);  //Swag Labs(0,1)
 		String actTitle=login.verifyTitleOfApplication();
-		AssertJUnit.assertEquals(expTitle, actTitle);
+		Assert.assertEquals(expTitle, actTitle);
 	}
 	@Test(enabled = true)
 	public void LoginToApplicationTest() throws IOException
 	{
 		String expURL=ReadData.readExcel_login(0, 2);  //https://www.saucedemo.com/inventory.html
 		String actURL=login.LoginToApplication();
-		AssertJUnit.assertEquals(expURL, actURL);
+		Assert.assertEquals(expURL, actURL);
 	}
 	@AfterMethod(alwaysRun = true)
 	public void closeBrowser(ITestResult it) throws IOException
